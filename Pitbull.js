73c3337116a7a35e05655dd99ebad2172ed7640d
Paul Saya <87011258+PaulSaya@users.noreply.github.com>
2022-11-15 15:04:10 +0200

    Upload new files

---
+++ Pitbull.js
@@ -0,0 +1,38 @@
+<!Doctype Html> 
+
+<style> 
+	font-family: Inverted Square;
+	font-size: 60px;
+	
+	.box {
+		background-image: url('/pix/samples/bg2.png');
+	}
+	div {
+		width: 20vw;
+		padding: 3vw;
+		margin: 2vw;
+		border: solid;		
+	}
+</style>
+
+
+
+<h2> Pitbull </h2> 
+
+<hr><br>
+<li>Helpful </li>
+<li> strong </li>
+<li>Immortalic </li>
+<li> behaviour improvements methods </li>
+<p2> Some creatures are scary some people only get these when there is something wrong in the family for instance if there was a protected. Next when there is something to be aware of 
+when buying a pitbull it not to put it with a Jack Russell they are small and can not defend themselves from a possible attack.</p2> 
+
+<h2> History of pitbull  </h2> 
+<hr><br>
+<h2> Personal Pointer </h2>
+<p2> I can tell immediatly that the creatures are violent but can listen to the owner based on listening back but if you do not allow that animal to be free it may can boring for it to do whta it whats. </p2> 
+<br>
+<img src = "https://upload.wikimedia.org/wikipedia/commons/1/17/Augustwiki.jpg"/> 
+
+<cite> Copyright 2022  Paul Saya </cite>
+</html>
